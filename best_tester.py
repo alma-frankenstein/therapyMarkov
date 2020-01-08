6d7725b4b15570015bfef757919d0fb9ecaf126a
Alma Frankenstein <48488886+alma-frankenstein@users.noreply.github.com>
2020-01-07 17:59:36 -0800

    with markovDict function

--- best_tester.py
+++ best_tester.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@authors: alma, justin
+@authors: Alma, Justin
 """
+#creates dictionary of words in the text as keys, followed by a nested 
+#dictionary of the words that follow it as keys and the number of times
+#they follow it as values
 
-wordSequences = {}
-
-with open('therapy_statements.txt') as file_object:
-    word = ""
-    for line in file_object:
-        #nextWord is the word you're on
-        for nextWord in line.split():
-            if word in wordSequences:
-                frequency = wordSequences[word]
-            else:
-                frequency = {}
-                wordSequences[word] = frequency
-            if nextWord in frequency:
-                 occurrances = frequency[nextWord]
-                 #value; either the number that's already there or the one we just put there
-            else:
-                occurrances = 0
-            frequency[nextWord] = occurrances + 1
-            word = nextWord
-    return wordSequences
-     
-
+def markovDict():
+    wordSequences = {}
+    
+    with open('therapy_statements.txt') as file_object:
+        #pastword is key in outer dict
+        pastWord = ""
+        for line in file_object:
+            #nextWord is the word you're on
+            for nextWord in line.split():
+                if pastWord in wordSequences:
+                    frequency = wordSequences[pastWord]
+                else:
+                    frequency = {}
+                    wordSequences[pastWord] = frequency
+                if nextWord in frequency:
+                     occurrances = frequency[nextWord]
+                     #value; either the number that's already there or the one we just put there
+                else:
+                    occurrances = 0
+                frequency[nextWord] = occurrances + 1
+                pastWord = nextWord
+        return wordSequences
 
