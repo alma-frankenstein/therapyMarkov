7b016b336b5eca0534e1c898e4020d5751ccb98d
Alma Frankenstein <48488886+alma-frankenstein@users.noreply.github.com>
2020-01-07 17:53:56 -0800

    Update best_tester.py

--- best_tester.py
+++ best_tester.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug  4 20:02:53 2019
-
 @authors: alma, justin
 """
 
 wordSequences = {}
 
-with open('greenEggs.txt') as file_object:
+with open('therapy_statements.txt') as file_object:
     word = ""
     for line in file_object:
         #nextWord is the word you're on
@@ -25,9 +23,7 @@ with open('greenEggs.txt') as file_object:
                 occurrances = 0
             frequency[nextWord] = occurrances + 1
             word = nextWord
-print(wordSequences)
+    return wordSequences
      
 
-#dict = {'meow':'cat', 'sup':'bro'}
 
-#print(dict['cat'])
